b3c16e76695552bf453009027edd20ff3bf65ff4
ozzyoss77 <crhistian.riverin@gmail.com>
2022-09-02 12:48:43 -0400

    Ejercicio 1 tema 3

---
+++ Tema 3/ejercicio1.py
@@ -0,0 +1,23 @@
+name = 'Pepillo'
+lastName = 'Grillo'
+age = 25
+email = 'pepillogrillo@gmail.com'
+phoneNumber = 654987321
+married = True
+withChilds = True
+listOfFriends = ['Laura', 'Ross', 'Cristian', 'Felipe', 'Leo', 'Brian']
+viewedMovies = {
+    'SciFi': 'Interestelar',
+    'Romance': 'Titanic',
+    'Anime': 'Dragon Ball'
+}
+
+
+print(name)
+print(lastName)
+print(email)
+print(phoneNumber)
+print(married)
+print(withChilds)
+print(listOfFriends)
+print(viewedMovies)
