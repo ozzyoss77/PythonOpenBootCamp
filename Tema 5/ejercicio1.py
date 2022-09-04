2976b7585a6cc7604408f9bdd3f59fb660b11303
ozzyoss77 <crhistian.riverin@gmail.com>
2022-09-04 16:08:47 -0400

    ejercicio 1 tema 5

--- Tema 5/ejercicio1.py
+++ Tema 5/ejercicio1.py
@@ -3,8 +3,10 @@ import math
 #Cáculo de un triángulo
 
 areaTri = lambda alturaTri, baseTri: print(round((alturaTri * baseTri) / 2, 2))
+areaCir = lambda radioCir: print(round(math.pi * (radioCir**2), 2))
 
-areaCir = lambda radioCir: print(round(math.pi * radioCir**2, 2))
+
 
 areaTri(alturaTri = float(input('Dime la altura del triángulo: ')), baseTri = float(input('Dime la base del triángulo: ')) )
+
 areaCir(radioCir=float(input('Dime el radio del circulo: ')))
