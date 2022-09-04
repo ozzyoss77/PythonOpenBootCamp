6a97182c69e2d228573f9ba775476efe86a1a172
ozzyoss77 <crhistian.riverin@gmail.com>
2022-09-04 13:28:58 -0400

    ejercicio 2 tema 4

---
+++ Tema 4/ejercicio2.py
@@ -0,0 +1,9 @@
+from typing import final
+
+
+initialNUmber = int(input('Ingresa un número por favor: '))
+finalNumber = int(input('Ingresa un número mas alto que el anterior: '))
+
+for number in range(initialNUmber, (finalNumber + 1)):
+    if number % 2 != 0:
+        print(number)
